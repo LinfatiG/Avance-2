47bf6ef050ccd5b4bfedf2b970b1154caf64a911
LinfatiG <43256414+LinfatiG@users.noreply.github.com>
2019-06-05 15:21:37 -0400

    Update Carta.java

--- Avance2/src/datoss/Carta.java
+++ Avance2/src/datoss/Carta.java
@@ -1,36 +1,24 @@
 package datoss;
 
-import Validaciones.Validador;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Scanner;
-
 public class Carta {
 
-    public void mostrarCarta() {
-        System.out.println("Menu de comestibles");
-        mostrarComestibles();
-        System.out.println("Menu de bebestibles");
-        mostrarBebestibles();
-    }
+    private int contador = 1;
 
-    public void mostrarComestibles() {
-        System.out.println("1.- Hamburguesas");
-        System.out.println("2.- Pollo con papas");
-        System.out.println("3.- Carne a la olla");
-        System.out.println("4.- Pizza Napolitana");
-        System.out.println("5.- Pizza vegetariana");
+    public void mostrarComestible() {
+        // int contador = 1;
+        for (Comestible com : Comestible.values()) {
+            System.out.println(contador + ".-" + com.toString() + "-");
+            contador++;
+        }
+
     }
 
     public void mostrarBebestibles() {
-        System.out.println("1.- Bebida");
-        System.out.println("2.- Nectar");
-        System.out.println("3.- Jugo natural");
-        System.out.println("4.- Agua mineral");
-    }
+        // int contador = 1;
+        for (Bebestible bes : Bebestible.values()) {
+            System.out.println(contador + ".-" + bes.toString() + "-");
+            contador++;
+        }
 
+    }
 }
